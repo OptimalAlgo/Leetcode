8997b6af76f6a2142426c08976d72c5980ce813a
OptimalAlgo <133063499+OptimalAlgo@users.noreply.github.com>
2023-07-10 16:03:22 +0100

    Solution for 1. Two Sum

---
+++ Solutions/1. Two Sum.py
@@ -0,0 +1,16 @@
+'''
+https://leetcode.com/problems/two-sum/
+Time: O(n)
+Space: O(n)
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complements = {}
+        for i, n in enumerate(nums):
+            complement = target - n
+            if n in complements:
+                return [complements[n], i]
+            complements[complement] = i
+
+        raise Exception("No solution was found")
